09bdc725bca72fa5995bae54cab97a405e4d3d5e
kid1980 <87967539+kid1980@users.noreply.github.com>
2024-07-30 23:39:05 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '莫忘初衷的每一天', url: 'https://post.ponien.com' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
