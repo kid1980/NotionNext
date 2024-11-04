6492a006738a1634348a22581b9014ff8b280a3b
kid1980 <87967539+kid1980@users.noreply.github.com>
2024-11-04 16:16:16 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -21,8 +21,8 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可/images/starter/hero/hero-image.webp ''
-  STARTER_HERO_PREVIEW_IMAGE: ' /images/starter/hero/hero-image.webp/ 产品预览图 ，默认读取public目录下图片
-  STARTER_HERO_BANNER_IMAGE: '/images/starter/hero/banner2024.webp / hero区下方的全宽图
+  STARTER_HERO_PREVIEW_IMAGE: ' /images/starter/hero/hero-image.webp',  // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_BANNER_IMAGE: '/ / hero区下方的全宽图
 
   // 顶部右侧导航暗流
   STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
